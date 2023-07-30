02be21e194f9a021b44a71ef1fc81cf82ea4a6d2
Juan Ignacio Marcos Merlo <juanimarcosmerlo@gmail.com>
2023-07-30 00:39:31 -0300

    agregada nuevas funciones y modal a la calculadora

--- JS/calculator.js
+++ JS/calculator.js
@@ -1,6 +1,13 @@
 let display = document.getElementById("display");
 let operador = "";
 let numeros = [];
+let totalResultado = 0;
+const historyModal = document.getElementById("historyModal");
+const operationHistory = document.getElementById("operationHistory");
+const finalResultDisplay = document.getElementById("finalResult");
+const operacionesRealizadas = [];
+
+
 
 window.addEventListener('load', function() {
     let resultadoGuardado = localStorage.getItem('resultado');
@@ -10,8 +17,69 @@ window.addEventListener('load', function() {
     }
 });
 
+function openModal() {
+    historyModal.style.display = "block";
+}
+function closeModal() {
+    historyModal.style.display = "none";
+}
+
+const historyBtn = document.createElement("button");
+historyBtn.id = "historyBtn";
+historyBtn.textContent = "Ver Historial";
+historyBtn.onclick = openModal;
+document.body.appendChild(historyBtn);
+
+
+
+function agregarOperacionAlHistorial(operacion) {
+    let li = document.createElement("li");
+    li.textContent = operacion;
+    li.onclick = function() {
+        const index = Array.from(operationHistory.childNodes).indexOf(li);
+        eliminarOperacion(index);
+    };
+    operationHistory.appendChild(li);
+    
+    let valorOperacion = eval(operacion);
+    operacionesRealizadas.push({ operacion, valorOperacion });
+
+    totalResultado += valorOperacion;
+    finalResultDisplay.textContent = totalResultado;
+}
+
+function eliminarOperacion(index) {
+    if (index >= 0 && index < numeros.length) {
+        operationHistory.removeChild(operationHistory.childNodes[index]);
+        let operacionEliminar = numeros[index];
+        if (index > 0) {
+            operacionEliminar = `${numeros[index - 1]} ${operador} ${numeros[index]}`;
+        }
+        let valorEliminar = eval(operacionEliminar);
+        totalResultado -= valorEliminar;
+        finalResultDisplay.textContent = totalResultado;
+        numeros.splice(index - 1, 2);
+    }
+}
+
+function eliminarOperacion(index) {
+    if (index >= 0 && index < operacionesRealizadas.length) {
+        operationHistory.removeChild(operationHistory.childNodes[index]);
+        let valorEliminar = operacionesRealizadas[index].valorOperacion;
+        totalResultado -= valorEliminar;
+        finalResultDisplay.textContent = totalResultado;
+        operacionesRealizadas.splice(index, 1);
+    }
+}
+
+
+
 function agregarNumero(numero) {
     display.value += numero;
+}
+function borrarUltimoCaracter() {
+    let valorActual = display.value;
+    display.value = valorActual.slice(0, -1);
 }
 
 function agregarOperador(op) {
@@ -44,6 +112,7 @@ function calcular() {
             alert("Operador inválido.");
             return;
         }
+        agregarOperacionAlHistorial(numeros.join(` ${operador} `));
         display.value = resultado;
         operador = "";
         numeros = [];
@@ -51,6 +120,8 @@ function calcular() {
         localStorage.setItem('resultado', JSON.stringify(resultadoObj));
 }
 
+
+// mis funciones
 
 function sumar() {
     let total = 0;
@@ -101,27 +172,63 @@ function potencia() {
     return Math.pow(base, exponente);
 }
 
+function resetearModal() {
+    while (operationHistory.firstChild) {
+        operationHistory.removeChild(operationHistory.firstChild);
+    }
+    totalResultado = 0;
+    finalResultDisplay.textContent = totalResultado;
+    operacionesRealizadas.length = 0;
+}
 
 function borrar() {
     display.value = "";
     operador = "";
     numeros = [];
+
+    resetearModal();
 }
 
 
+//eventos de teclado
 
-document.addEventListener("keydown", function(event) {
-    let tecla = event.key;
+
+let altGrKeyPressed = false;
+
+  document.addEventListener("keydown", function(e) {
+    let tecla = e.key;
   
-    if (!isNaN(tecla)) {
-      agregarNumero(tecla);
-    } else if (tecla === "+" || tecla === "-" || tecla === "*" || tecla === "/") {
-      agregarOperador(tecla);
-    } else if (tecla === "Enter" || "intro") {
-      calcular();
-    } else if (tecla === "Backspace") {
-      borrar();
+    if (e.altKey && tecla === "Control") {
+        altGrKeyPressed = true;
     }
-  });
+
+
+    if (!isNaN(tecla)) {
+        agregarNumero(tecla);
+    } else if (tecla === "+" || tecla === "-" || tecla === "*" || tecla === "/") {
+        agregarOperador(tecla);
+    } else if (tecla === "Enter" || tecla === "Intro") {
+        calcular();
+    } else if (tecla === ".") {
+        agregarDecimal();
+    } else if (tecla === "Escape") {
+        borrar();
+    } else if (tecla === "Backspace") {
+        borrarUltimoCaracter();
+    } else if (tecla === "e") {
+        display.value = Math.E;
+    } else if (tecla === "p") {
+        display.value = Math.PI;
+    } else if (altGrKeyPressed && tecla === "^") {
+        agregarOperador("^");
+    }
+});
+
+document.addEventListener("keyup", function(e) {
+    if (e.key === "Control") {
+        altGrKeyPressed = false;
+    }
+});
+
 
 
